47a681e21b6b0b83b2b5a5e3353e6076c9932c30
freeazeMC <berkcxnprivmail@gmail.com>
2026-07-06 01:23:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+import { db } from "./firebase.js";
+import { collection, addDoc, onSnapshot, query, orderBy } from "firebase/firestore";
 
+const feed = document.getElementById('feed');
+
+// Verileri anlık çek ve listele
+const q = query(collection(db, "paylasimlar"), orderBy("tarih", "desc"));
+onSnapshot(q, (snapshot) => {
+    feed.innerHTML = "";
+    snapshot.forEach((doc) => {
+        const data = doc.data();
+        feed.innerHTML += `
+            <div class="post">
+                <div class="user-info">
+                    <div class="avatar"></div>
+                    <span>Anonim Bey</span>
+                </div>
+                <p>${data.icerik}</p>
+                <small>${new Date(data.tarih).toLocaleString()}</small>
+            </div>
+        `;
+    });
+});
